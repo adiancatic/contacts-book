3430d6936949d8ea3c3260b5735b9f996603a457
adian <adiancatic@gmail.com>
2021-03-21 18:17:39 +0100

    ADD: Added basic logic for fetching new contact's form data and storing it in local storage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+const CONTACTS = 'contacts';
+const contactForm = $('#add-contact-form');
+
+
 $(document).ready(() => {
     handleInputPanelToggle();
+
+    if(!localStorage[CONTACTS]) {
+        localStorage.setItem(CONTACTS, JSON.stringify([]));
+    }
+
+    contactForm.on('submit', function (e) {
+        e.preventDefault();
+    });
+
+
 });
+
+
+
+
 
 function handleInputPanelToggle() {
     const toggle = $('.input-panel-toggle');
@@ -11,4 +29,29 @@ function handleInputPanelToggle() {
         backdrop.fadeToggle();
         inputPanel.toggleClass('js-active');
     })
+}
+
+function addContact() {
+    const contact = getNewContact();
+    storeContact(contact);
+}
+
+function getNewContact() {
+    let contact = {};
+    let formData = contactForm.serializeArray();
+    for (const key in formData) {
+        const data = formData[key];
+        contact[data.name] = data.value;
+    }
+    return contact;
+}
+
+function storeContact(contact) {
+    if(contact === {} || !localStorage.getItem(CONTACTS)) {
+        return;
+    }
+    let contacts = JSON.parse(localStorage.getItem(CONTACTS));
+    contacts.push(contact);
+    localStorage.setItem(CONTACTS, JSON.stringify(contacts));
+    console.log(JSON.parse(localStorage.getItem(CONTACTS)))
 }
